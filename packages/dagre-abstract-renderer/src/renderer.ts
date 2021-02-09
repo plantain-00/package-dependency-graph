d4667bf3060c1baae97292f8cdf5d6dfb3eb36e4
York Yao <me@yorkyao.com>
2021-02-10 06:36:59 +0800

    feat: improve dagre edge arrow style

--- packages/dagre-abstract-renderer/src/renderer.ts
+++ packages/dagre-abstract-renderer/src/renderer.ts
@@ -21,6 +21,27 @@ export function renderDagre<T>(graph: dagre.graphlib.Graph, target: RenderTarget
     const edgeValue: GraphEdgeValue = graph.edge(edge)
     const edgeColor = edgeValue.color!
     const points = edgeValue.points.map((p) => ({ x: p.x + margin, y: p.y + margin }))
+
+    const point1 = points[points.length - 2]
+    const point2 = points[points.length - 1]
+    const arrowSize = 10
+    const deltaAlpha = Math.PI / 8
+    const alpha = Math.atan((point2.x - point1.x) / (point1.y - point2.y))
+    const alpha1 = alpha - deltaAlpha
+    const alpha2 = alpha + deltaAlpha
+    const arrow1 = {
+      x: point2.x + arrowSize * Math.sin(alpha1),
+      y: point2.y - arrowSize * Math.cos(alpha1)
+    }
+    const arrow2 = {
+      x: point2.x + arrowSize * Math.sin(alpha2),
+      y: point2.y - arrowSize * Math.cos(alpha2)
+    }
+    points[points.length - 1] = {
+      x: (arrow1.x + arrow2.x) / 2,
+      y: (arrow1.y + arrow2.y) / 2,
+    }
+
     if (points.length === 3) {
       children.push(target.quadraticCurveTo(points[0], points[1], points[2], edgeColor))
     } else if (points.length === 4) {
@@ -31,35 +52,11 @@ export function renderDagre<T>(graph: dagre.graphlib.Graph, target: RenderTarget
     } else {
       children.push(target.polyline(points, edgeColor))
     }
-    let point1 = edgeValue.points[edgeValue.points.length - 2]
-    const point2 = edgeValue.points[edgeValue.points.length - 1]
-    const arrowSize = 10
-    const deltaAlpha = Math.PI / 8
-    if (edgeValue.points.length >= 3) {
-      const point0 = edgeValue.points[edgeValue.points.length - 3]
-      const rate = arrowSize / Math.sqrt(Math.pow(point0.x - point2.x, 2) + Math.pow(point0.y - point2.y, 2))
-      const remainRate = 1 - rate
-      point1 = {
-        x: Math.pow(remainRate, 2) * point2.x + 2 * rate * remainRate * point1.x + Math.pow(rate, 2) * point0.x,
-        y: Math.pow(remainRate, 2) * point2.y + 2 * rate * remainRate * point1.y + Math.pow(rate, 2) * point0.y,
-      }
-    }
-    const alpha = Math.atan((point2.x - point1.x) / (point1.y - point2.y))
-    const alpha1 = alpha - deltaAlpha
-    const alpha2 = alpha + deltaAlpha
+    
     children.push(target.polygon([
-      {
-        x: point2.x + margin,
-        y: point2.y + margin
-      },
-      {
-        x: point2.x + arrowSize * Math.sin(alpha1) + margin,
-        y: point2.y - arrowSize * Math.cos(alpha1) + margin
-      },
-      {
-        x: point2.x + arrowSize * Math.sin(alpha2) + margin,
-        y: point2.y - arrowSize * Math.cos(alpha2) + margin
-      }
+      point2,
+      arrow1,
+      arrow2,
     ], edgeColor))
   }
   for (const node of graph.nodes()) {
